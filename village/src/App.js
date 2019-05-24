be1b4605a6129b89d838f82e4659020417c422a1
angel prats <angelpratz@gmail.com>
2019-05-24 16:12:52 -0400

    added links and created navigation route cle

--- village/src/App.js
+++ village/src/App.js
@@ -5,6 +5,7 @@ import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 import axios from 'axios';
 import { Route } from "react-router-dom"
+import Navigation from './components/Navigation';
 
 
 class App extends Component {
@@ -27,19 +28,19 @@ class App extends Component {
       .catch(error => console.log(error));
   }
 
-  addSmurf = (event, smurf) => {
-    event.preventDefault();
-    axios
-      .post('http://localhost:3333/smurfs', smurf)
-      .then(res => {
-        this.setState({
-          smurfs: res.data
-        })
-      })
-      .catch(err => {
-        console.log(err)
-      });
-  }
+  // addSmurf = (event, smurf) => {
+  //   event.preventDefault();
+  //   axios
+  //     .post('http://localhost:3333/smurfs', smurf)
+  //     .then(res => {
+  //       this.setState({
+  //         smurfs: res.data
+  //       })
+  //     })
+  //     .catch(err => {
+  //       console.log(err)
+  //     });
+  // }
 
 
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -49,16 +50,18 @@ class App extends Component {
     return (
       <div className="App">
 
+        <Route path='/' component={Navigation} />
         <Route
-          exact path="/smurf"
+          exact path="/smurfs"
           render={props => <Smurfs {...props} smurfs={this.state.smurfs} />}
         />
 
         <Route
           path="/smurf-form"
-          render={props => <SmurfForm {...props} smurfs={this.state.smurfs}
-            addSmurf={this.addSmurf} />}
+          render={props => <SmurfForm {...props} />}
+
         />
+
       </div>
     );
   }
---
+++ village/src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <div>
+
+      <Link className="navigation" to="/smurfs">The SMURFS </Link>
+      <br />
+      <Link to="/smurfs" className='navigation'> Smurfs </Link>
+      <br />
+      <Link to="/smurf-form" className='navigation'>Add A Smurf </Link>
+    </div>
+
+  );
+};
+
+
+export default Navigation;
