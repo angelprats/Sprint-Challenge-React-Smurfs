fcbc42f5e125cee2983aa09fe67c4ae5cb6254e8
angel prats <angelpratz@gmail.com>
2019-05-24 12:00:24 -0400

    constructed axios post

--- village/src/App.js
+++ village/src/App.js
@@ -19,6 +19,22 @@ class App extends Component {
       .then(res => this.SetState({ smurfs: res.data }))
       .catch(error => console.log(error));
   }
+
+  addSmurf = (event, smurf) => {
+    event.preventDefault();
+    axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+        this.setState({
+          smurfs: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
